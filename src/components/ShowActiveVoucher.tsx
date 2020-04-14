db0cbbd32bed0de35651d7cc8e07a49ed7b3b764
kaspervh <kaspervhauschildt@protonmail.com>
2020-04-14 11:24:41 +0200

    made component

---
+++ src/components/ShowActiveVoucher.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const ShowActiveVoucher = () => {
+  return(
+    <View>
+
+      <Text>Diller</Text>
+    </View>
+  )
+}
+
+export default ShowActiveVoucher;
